fece1fa60aa9703370f11cc3d908450469858cb2
Jimmy <d08922002@csie.ntu.edu.tw>
2020-04-11 15:39:42 +0800

    add hw2.py

---
+++ HW2/HW2.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## HW2: Linear Discriminant Analysis
+# In hw2, you need to implement Fisher’s linear discriminant by using only numpy, then train your implemented model by the provided dataset and test the performance with testing data
+# 
+# Please note that only **NUMPY** can be used to implement your model, you will get no points by simply calling sklearn.discriminant_analysis.LinearDiscriminantAnalysis 
+
+# ## Load data
+import pandas as pd
+
+x_train = pd.read_csv("x_train.csv").values
+y_train = pd.read_csv("y_train.csv").values[:,0]
+x_test = pd.read_csv("x_test.csv").values
+y_test = pd.read_csv("y_test.csv").values[:,0]
+
+print(x_train.shape)
+print(y_train.shape)
+print(x_test.shape)
+print(y_test.shape)
+
+
+# ## 1. Compute the mean vectors mi, (i=1,2) of each 2 classes
+
+## Your code HERE
+
+
+assert m1.shape == (2,)
+print(f"mean vector of class 1: {m1}", f"mean vector of class 2: {m2}")
+
+
+# ## 2. Compute the Within-class scatter matrix SW
+
+## Your code HERE
+
+
+assert sw.shape == (2,2)
+print(f"Within-class scatter matrix SW: {sw}")
+
+
+# ## 3.  Compute the Between-class scatter matrix SB
+
+## Your code HERE
+
+
+assert sb.shape == (2,2)
+print(f"Between-class scatter matrix SB: {sb}")
+
+
+# ## 4. Compute the Fisher’s linear discriminant
+
+## Your code HERE
+
+
+assert w.shape == (2,1)
+print(f" Fisher’s linear discriminant: {w}")
+
+
+
+# ## 5. Project the test data by linear discriminant to get the class prediction by nearest-neighbor rule and calculate the accuracy score 
+# you can use accuracy_score function from sklearn.metric.accuracy_score
+
+acc = accuracy(y_test, y_pred)
+
+print(f"Accuracy of test-set {acc}")
+
+
+# ## 6. Plot the 1) projection line 2) Decision boundary and colorize the data with each class
+# ### the result should be look like this [image](https://i2.kknews.cc/SIG=fe79fb/26q1000on37o7874879n.jpg) (Red line: projection line, Green line: Decision boundary)
+
+
+
