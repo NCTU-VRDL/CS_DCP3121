68aac4a50ce749eed9258d584ddf259d54901423
Jimmy <d08922002@csie.ntu.edu.tw>
2020-05-26 02:01:42 +0800

    update HW4

--- HW4/HW4.py
+++ HW4/HW4.py
@@ -10,7 +10,6 @@ y_train = np.load("y_train.npy")
 x_test = np.load("x_test.npy")
 y_test = np.load("y_test.npy")
 
-
 # 550 data with 300 features
 print(x_train.shape)
 
@@ -19,15 +18,31 @@ print(np.unique(y_train))
 
 
 # ## Question 1
-# Implement the K-fold cross-validation function. Your function should take K as an argument and return a list (len(list) should equal to K), contains K elements, each element is a list contains the training fold and validation fold
+# K-fold data partition: Implement the K-fold cross-validation function. Your function should take K as an argument and return a list of lists (len(list) should equal to K), which contains K elements. Each element is a list contains two parts, the first part contains the index of all training folds, e.g. Fold 2 to Fold 5 in split 1. The second part contains the index of validation fold, e.g. Fold 1 in  split 1
 
 def cross_validation(x_train, y_train, k=5):
     return NotImplementedError
 
+
 kfold_data = cross_validation(x_train, y_train, k=10)
 assert len(kfold_data) == 10 # should contain 10 fold of data
 assert len(kfold_data[0]) == 2 # each element should contain train fold and validation fold
-assert len(kfold_data[0][0].shape[0]) == 90 # The number of data in each fold should equal to training data divieded by K
+assert kfold_data[0][1].shape[0] == 55 # The number of data in each validation fold should equal to training data divieded by K
+
+
+# ## example
+from sklearn.model_selection import KFold
+
+X = np.arange(20)
+kf = KFold(n_splits=5, shuffle=True)
+kfold_data= []
+for i, (train_index, val_index) in enumerate(kf.split(X)):
+    print("Split: %s, Training index: %s, Validation index: %s" % (i+1, train_index, val_index))
+    kfold_data.append([train_index, val_index])
+
+assert len(kfold_data) == 5 # should contain 5 fold of data
+assert len(kfold_data[0]) == 2 # each element should contains index of training fold and validation fold
+assert kfold_data[0][1].shape[0] == 4 # The number of data in each validation fold should equal to training data divieded by K
 
 
 # ## Question 2
@@ -35,28 +50,24 @@ assert len(kfold_data[0][0].shape[0]) == 90 # The number of data in each fold sh
 
 clf = SVC(C=1.0, kernel='rbf', gamma=0.01)
 
-## your code
-
-
 print(best_parameters)
 
 
 # ## Question 3
-# Plot the grid search results of your SVM. The x, y, z represent the parameters of “C”, “kernel” and “gamma”, respectively. And the color represents the average score of validation folds.
-# You reults should be same as the reference image ![image](https://plotly.com/~xoelop/147.png) 
+# Plot the grid search results of your SVM. The x, y represents the hyperparameters of “gamma” and “C”, respectively. And the color represents the average score of validation folds
+# You reults should be same as the reference image ![image](https://miro.medium.com/max/1296/1*wGWTup9r4cVytB5MOnsjdQ.png) 
 
 # ## Question 4
-# Train your SVM model by the best parameters you found from question 2 on the whole training set and evaluate the performance on the test set. **You accuracy should over 0.8**
-
+# Train your SVM model by the best parameters you found from question 2 on the whole training set and evaluate the performance on the test set. **You accuracy should over 0.85**
 
 y_pred = best_model.predict(x_test)
 print("Accuracy score: ", accuracy_score(y_pred, y_test))
 
 
 # ## Question 5
-# Compare the performance of each model you have implemented from HW1 to HW3
+# Compare the performance of each model you have implemented from HW1
 
-
+# ### HW1
 train_df = pd.read_csv("../HW1/train_data.csv")
 x_train = train_df['x_train'].to_numpy().reshape(-1,1)
 y_train = train_df['y_train'].to_numpy().reshape(-1,1)
@@ -65,30 +76,6 @@ test_df = pd.read_csv("../HW1/test_data.csv")
 x_test = test_df['x_test'].to_numpy().reshape(-1,1)
 y_test = test_df['y_test'].to_numpy().reshape(-1,1)
 
-
 print("Square error of Linear regression: ")
 print("Square error of SVM regresssion model: ")
-
-
-x_train = pd.read_csv("../HW2/x_train.csv").values
-y_train = pd.read_csv("../HW2/y_train.csv").values[:,0]
-x_test = pd.read_csv("../HW2/x_test.csv").values
-y_test = pd.read_csv("../HW2/y_test.csv").values[:,0]
-
-
-print("Accuracy of FLD: ")
-print("Accuracy SVM classification model: ")
-
-
-
-x_train = pd.read_csv("../HW3/x_train.csv")
-y_train = pd.read_csv("../HW3/y_train.csv")
-x_test = pd.read_csv("../HW3/x_test.csv")
-y_test = pd.read_csv("../HW3/y_test.csv")
-
-
-print("Accuracy of Decision Tree: ")
-print("Accuracy of Random Forest: ")
-print("Accuracy SVM classification model: ")
-
 
